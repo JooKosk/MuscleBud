5cc415d9750614715e86f18c5b9b2eb12de33001
Joonas Koskela <Jomppe.koskela@gmail.com>
2020-11-27 16:31:33 +0200

    initial commit

---
+++ client/build/static/js/main.99d7fc1a.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);var c=n(1),i=n(0),r=n.n(i),s=n(3),o=function(){return Object(c.jsx)("div",{children:"hello"})};n.n(s).a.render(Object(c.jsx)(r.a.StrictMode,{children:Object(c.jsx)(o,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.99d7fc1a.chunk.js.map
